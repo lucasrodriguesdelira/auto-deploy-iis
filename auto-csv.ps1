a81986d88066d541564564929c782c332f8aaaec
lucasrodriguesdelira <54848684+lucasrodriguesdelira@users.noreply.github.com>
2021-09-01 16:31:36 -0300

    Update auto-csv.ps1

--- auto-csv.ps1
+++ auto-csv.ps1
@@ -2,7 +2,11 @@ $SERVIDOR = $env:SERVIDOR
 $ORIGEM   = $env:ORIGEM
 $DESTINO  = $env:DESTINO
 
-"Servidor;Origem;Destino" > listDeploy.csv
-For ($i=0; $i -lt $SERVIDOR.count; $i++) {
-    $SERVIDOR[$i],$ORIGEM[$i],$DESTINO[$i] -join ';' >> listDeploy.csv
-}
+#"Servidor;Origem;Destino" > listDeploy.csv
+#For ($i=0; $i -lt $SERVIDOR.count; $i++) {
+#    $SERVIDOR[$i],$ORIGEM[$i],$DESTINO[$i] -join ';' #>> listDeploy.csv
+#}
+
+"$SERVIDOR"
+"$ORIGEM"
+"$DESTINO"
