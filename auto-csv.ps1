a47dfaaff4caec51397dfda0f848899d9d26fa65
lucasrodriguesdelira <54848684+lucasrodriguesdelira@users.noreply.github.com>
2021-09-01 17:32:35 -0300

    Update auto-csv.ps1

--- auto-csv.ps1
+++ auto-csv.ps1
@@ -1,12 +1,11 @@
-"$env:SERVIDOR" > SERVIDOR.txt
-"$env:ORIGEM" > ORIGEM.txt
-"$env:DESTINO" > DESTINO.txt
+"$env:SERVIDOR" -replace ',',"`n" > SERVIDOR.txt
+"$env:ORIGEM" -replace ',',"`n" > ORIGEM.txt
+"$env:DESTINO" -replace ',',"`n" > DESTINO.txt
 
 $SERVIDOR = Get-Content SERVIDOR.txt
 $ORIGEM = Get-Content ORIGEM.txt
 $DESTINO = Get-Content DESTINO.txt
 
-"Servidor,Origem,Destino" > listDeploy.txt
 For ($i=0; $i -lt $SERVIDOR.count; $i++) {
     $SERVIDOR[$i],$ORIGEM[$i],$DESTINO[$i] -join ','
 }
